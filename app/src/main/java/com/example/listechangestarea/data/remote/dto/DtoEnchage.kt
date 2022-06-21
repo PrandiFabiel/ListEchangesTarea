86f14fd4f55526ccd535024508153b5d581f3325
PrandiFabiel <fabiel098@gmail.com>
2022-06-21 11:14:09 -0400

    Agregando dto de echanges

---
+++ app/src/main/java/com/example/listechangestarea/data/remote/dto/DtoEnchage.kt
@@ -0,0 +1,9 @@
+package com.example.listechangestarea.data.remote.dto
+
+data class DtoEnchage(
+    val id: String = "",
+    val name: String = "",
+    val description: String = "",
+    val active: Boolean = false,
+    val last_updated: String = ""
+)
